a9fbf5d61edb9a27098fcdf5100bed0d478dd14a
bogdan-vlad <32884764+bogdan-vlad@users.noreply.github.com>
2018-05-10 23:06:42 +0100

    Update settings.py

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -136,5 +136,5 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "bogdan.vlad.v@gmail.com"
-EMAIL_HOST_PASSWORD = "bogdanvf64"
+EMAIL_HOST_PASSWORD = "password"
 EMAIL_PORT = 587
